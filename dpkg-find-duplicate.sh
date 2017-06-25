033e50a5db069e90173e8f965af62cbf39e4b3e1
Florent Poinsaut <floSoX@users.noreply.github.com>
2017-06-25 20:40:00 +0200

    Create dpkg-find-duplicate.sh

---
+++ dpkg-find-duplicate.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+i=0
+IFS="
+"
+
+for line in $(dpkg -l | tail -n +6 | sed -e 's/\S\+\s\+\S\+\s\+\S\+\s\+\S\+\s\+\(.*\)/\1/g' | uniq -d)
+do
+  i=$(( $i + 1 ))
+	echo Packages with description: "$line"
+	dpkg -l | grep "$line" | sed -e 's/\(\S\+\s\+\S\+\s\+\S\+\s\+\S\+\s\+\).*/\1/g'
+	echo
+done
+
+echo "$i descriptions detected in more than one package."
