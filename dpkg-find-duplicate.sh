3786960e4a9ebaac6145578add39d4616f123902
Florent Poinsaut <florent@poinsaut.fr>
2021-02-19 23:20:32 +0100

    Fix SC2004

--- dpkg-find-duplicate.sh
+++ dpkg-find-duplicate.sh
@@ -6,7 +6,7 @@ IFS="
 
 for line in $(dpkg -l | tail -n +6 | sed -e 's/\S\+\s\+\S\+\s\+\S\+\s\+\S\+\s\+\(.*\)/\1/g' | uniq -d)
 do
-	i=$(( $i + 1 ))
+	i=$(( i + 1 ))
 	echo Packages with description: "$line"
 	dpkg -l | grep "$line" | sed -e 's/\(\S\+\s\+\S\+\s\+\S\+\s\+\S\+\s\+\).*/\1/g'
 	echo
